1f9524240e03847b51a6e0af2a8fa167967327a1
Caleb-Kronstad <caleb@calebkronstad.com>
2026-03-21 19:00:46 -0400

    added choosing cards and actions logic

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -6,8 +6,11 @@ public enum ActionType
 {
     NONE = 0,
     DMG_PLR,
+    DMG_PLR_BIG,
+    DMG_PLR_BIG_BIG,
+    SHUF_PLR,
     HEAL_BOSS,
-    END
+    END 
 }
 
 [System.Serializable]
@@ -16,21 +19,31 @@ public class Action
     public ActionType type = ActionType.NONE;
     public int cost = 0;
     public int action_value = 0;
+    public int frequency = 100;
+    public int since_last_use = 0;
 
-    public Action(ActionType t, int c, int av)
+    public Action(ActionType t, int c, int av, int f)
     {
         this.type = t;
         this.cost = c;
         this.action_value = av;
+        this.frequency = f;
     }
 }
 
 public class Boss : MonoBehaviour
 {
-    [SerializeField] private int max_health;
-    private int health;
+    private float timer = 0.0f;
 
     [SerializeField] private Player player;
+    
+    [SerializeField] private int max_health = 1000; // 777 later
+    private int health;
+    
+    [SerializeField] private int max_chips = 10;
+    private int chips;
+    [SerializeField] private int chip_regen_interval = 3;
+    [SerializeField] private int chip_regen_amount = 1;
 
     private Action[] all_actions;
     private ActionType last_action_type = ActionType.NONE;
@@ -38,28 +51,31 @@ public class Boss : MonoBehaviour
     void Start()
     {
         all_actions = new Action[Convert.ToInt32(ActionType.END)];
+        all_actions[Convert.ToInt32(ActionType.DMG_PLR)] = new Action(ActionType.DMG_PLR, 1, 5, 100);
+        all_actions[Convert.ToInt32(ActionType.DMG_PLR_BIG)] = new Action(ActionType.DMG_PLR_BIG, 4, 15, 60);
+        all_actions[Convert.ToInt32(ActionType.DMG_PLR_BIG_BIG)] = new Action(ActionType.DMG_PLR_BIG_BIG, 10, 50, 10);
+        all_actions[Convert.ToInt32(ActionType.HEAL_BOSS)] = new Action(ActionType.HEAL_BOSS, 1, 10, 100);
+        all_actions[Convert.ToInt32(ActionType.SHUF_PLR)] = new Action(ActionType.SHUF_PLR, 8, 0, 20);
     }
 
     void Update()
     {
-        
-    }
-    
-    public void Damage(int amount)
-    {
-        health -= amount;
-        health = Mathf.Clamp(health, 0, max_health);
-    }
-    public void Heal(int amount)
-    {
-        health += amount;
-        health = Mathf.Clamp(health, 0, max_health);
+        timer += Time.deltaTime;
+        int second = Mathf.RoundToInt(timer);
+
+        if (second % chip_regen_interval == 0) Earn(chip_regen_amount);
     }
 
     // Choose Action
     public Action Wethepeopleoftheunitedstatesinordertoformamoreperfectunionestablishjusticeinsuredomestictranquilityprovidforthecommondefencepromotethegeneralwelfareadnsecuretheblessingsoflibertytoourselvesandourposteritydoordainandestablishthisconstitutionfortheunitedstatesofamerica
         (Action action)
     {
+        for (int i = 0; i < all_actions.Length; i++)
+        {
+            all_actions[i].since_last_use++;
+        }
+        action.since_last_use = 0;
+        
         // perform current action
         switch (action.type)
         {
@@ -68,20 +84,79 @@ public class Boss : MonoBehaviour
             case ActionType.END:
                 break;
             case ActionType.DMG_PLR:
+            case ActionType.DMG_PLR_BIG: 
+            case  ActionType.DMG_PLR_BIG_BIG:
                 player.Damage(action.action_value);
                 break;
             case ActionType.HEAL_BOSS:
                 Heal(action.action_value);
                 break;
+            case ActionType.SHUF_PLR:
+                player.ShuffleCards();
+                break;
         }
         
         // ready next action
-        Action random_action = all_actions[0];
-        while (random_action.type == last_action_type)
-        {
-            random_action = all_actions[Random.Range(0, all_actions.Length)];
-        }
+        Action random_action = NextAction();
         last_action_type = random_action.type;
+        
+        Manage();
         return random_action;
+    }
+
+    private Action NextAction()
+    {
+        int total = 0;
+        for (int i = 1; i < all_actions.Length; i++)
+        {
+            if (all_actions[i] == null || all_actions[i].type == ActionType.NONE || all_actions[i].type == ActionType.END) continue;
+            
+            total += Convert.ToInt32(CalculateActionWeight(all_actions[i]));
+        }
+
+        int roll = Random.Range(0, total);
+        int cumulative = 0;
+        for (int i = 1; i < all_actions.Length; i++)
+        {
+            if (all_actions[i] == null || all_actions[i].type == ActionType.NONE || all_actions[i].type == ActionType.END) continue;
+            cumulative += Convert.ToInt32(CalculateActionWeight(all_actions[i]));
+            if (roll < cumulative) return all_actions[i];
+        }
+        return all_actions[1];
+    }
+
+    private float CalculateActionWeight(Action action)
+    {
+        float weight = action.frequency + action.since_last_use;
+        if (action.type == ActionType.HEAL_BOSS && (float)health / max_health < 0.5f)
+            weight += (int)(50.0f * Mathf.Pow(2, (1 - ((float)health/max_health)) * 4.0f));
+        if (action.type == ActionType.DMG_PLR_BIG_BIG && action.since_last_use > 10)
+            weight += 50;
+            
+        if (action.cost > chips || action.type == last_action_type) weight = 0.0f;
+        return weight;
+    }
+    
+    public void Damage(int amount)
+    {
+        health -= amount;
+    }
+    public void Heal(int amount)
+    {
+        health += amount;
+    }
+    public void Spend(int amount)
+    {
+        chips -=  amount;
+    }
+    public void Earn(int amount)
+    {
+        chips += amount;
+    }
+    
+    private void Manage()
+    {
+        health = Mathf.Clamp(health, 0, max_health);
+        chips = Mathf.Clamp(chips, 0, max_chips);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,7 +9,7 @@ public enum CardType
     DMG_BOSS,
     DMG_BOSS_BIG,
     SHUF,
-    GIEV_BLOONS,
+    GIVE_BLOONS,
     END
 }
 
@@ -33,29 +33,21 @@ public class Card
 public class Player : MonoBehaviour
 {
     private float timer = 0.0f;
-    
-    [SerializeField] private int max_health;
+
+    [SerializeField] private Boss boss;
+
+    [SerializeField] private int max_health = 100;
     private int health;
 
-    [SerializeField] private int max_bloons;
+    [SerializeField] private int max_bloons = 10;
     private int bloons;
     [SerializeField] private int bloon_regen_interval = 2;
     [SerializeField] private int bloon_regen_amount = 1;
-
-    [SerializeField] private Boss boss;
 
     [SerializeField] private int max_hand = 5;
     private Card[] hand;
     private Card[] all_cards;
     private CardType last_received_card_type;
-
-    private void Update()
-    {
-        timer += Time.deltaTime;
-        int second = Mathf.RoundToInt(timer);
-
-        if (second % bloon_regen_interval == 0) Earn(bloon_regen_amount);
-    }
 
     void Start()
     {
@@ -66,20 +58,17 @@ public class Player : MonoBehaviour
         all_cards[Convert.ToInt32(CardType.DMG_BOSS)] = new Card(CardType.DMG_BOSS, 2, 5, 100);
         all_cards[Convert.ToInt32(CardType.DMG_BOSS_BIG)] = new Card(CardType.DMG_BOSS_BIG, 3, 30, 100);
         all_cards[Convert.ToInt32(CardType.SHUF)] = new Card(CardType.SHUF, 3, 0, 100);
-        all_cards[Convert.ToInt32(CardType.GIEV_BLOONS)] = new Card(CardType.GIEV_BLOONS, 2, 5, 100);
+        all_cards[Convert.ToInt32(CardType.GIVE_BLOONS)] = new Card(CardType.GIVE_BLOONS, 2, 5, 100);
         
         ShuffleCards();
     }
 
-    public void ShuffleCards()
+    private void Update()
     {
-        Card[] random_hand =  new Card[max_hand];
-        for (int i = 0; i < max_hand; i++)
-        {
-            random_hand[i] = all_cards[Random.Range(0, all_cards.Length)];
-            last_received_card_type = random_hand[i].type;
-        }
-        hand = random_hand;
+        timer += Time.deltaTime;
+        int second = Mathf.RoundToInt(timer);
+
+        if (second % bloon_regen_interval == 0) Earn(bloon_regen_amount);
     }
 
     public Card ChooseCard(Card card)
@@ -100,21 +89,48 @@ public class Player : MonoBehaviour
             case CardType.SHUF:
                 ShuffleCards();
                 break;
-            case CardType.GIEV_BLOONS:
+            case CardType.GIVE_BLOONS:
                 Earn(card.action_value);
                 break;
         }
         Spend(card.cost);
 
-        Card random_card = all_cards[0];
-        while (random_card.type == last_received_card_type)
-        {
-            random_card = all_cards[Random.Range(0, all_cards.Length)];
-        }
+        Card random_card = NextCard();
         last_received_card_type = random_card.type;
         
         Manage();
         return random_card;
+    }
+    
+    private Card NextCard()
+    {
+        int total = 0;
+        for (int i = 1; i < all_cards.Length; i++)
+        {
+            if (all_cards[i] == null || all_cards[i].type == CardType.NONE || all_cards[i].type == CardType.END) continue;
+            total += all_cards[i].frequency;
+        }
+
+        int roll = Random.Range(0, total);
+        int cumulative = 0;
+        for (int i = 1; i < all_cards.Length; i++)
+        {
+            if (all_cards[i] == null || all_cards[i].type == CardType.NONE || all_cards[i].type == CardType.END) continue;
+            cumulative += all_cards[i].frequency;
+            if (roll < cumulative) return all_cards[i];
+        }
+        return all_cards[1];
+    }
+
+    public void ShuffleCards()
+    {
+        Card[] random_hand =  new Card[max_hand];
+        for (int i = 0; i < max_hand; i++)
+        {
+            random_hand[i] = NextCard();
+            last_received_card_type = random_hand[i].type;
+        }
+        hand = random_hand;
     }
     
     public void Damage(int amount)
@@ -138,5 +154,10 @@ public class Player : MonoBehaviour
     {
         health = Mathf.Clamp(health, 0, max_health);
         bloons = Mathf.Clamp(bloons, 0, max_bloons);
+    }
+
+    public Card[] GetHand()
+    {
+        return hand; 
     }
 }
